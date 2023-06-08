9da86dfc42e2e69c647f988763e793dbb2eea7e6
pablo014 <angelopablo014@gmail.com>
2023-06-08 16:48:43 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+</template>
+
+<style scoped>
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+</style>

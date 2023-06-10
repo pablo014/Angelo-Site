e38de6e1ea0781c9cf271560eda1cd9573528d50
pablo014 <angelopablo014@gmail.com>
2023-06-10 12:01:33 -0700

    feat(banner): update banner

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,18 +6,29 @@ import TheWelcome from '../components/TheWelcome.vue'
   <main>
       <div>
           <div class="image">
-          <img src="@/assets/angeloimg.png" class="static">
+              <div class="lg:ml-10">
+                  <h1 class="lg:text-7xl text-3xl font-bold underline text-primary">
+                      Hello, I'm Angelo!
+                  </h1>
+              </div>
+              <img src="@/assets/angeloimg.png">
           </div>
-          <h1 class="text-3xl font-bold underline text-primary">
-              Hello world!
-          </h1>
-          <h2 class="text-neutral">subtext</h2>
       </div>
   </main>
 </template>
 <style scoped>
 .image {
-    @apply bg-gradient-to-tl from-secondary to-primarydark;
-    border-radius: 0px 0px 0px 500px;
+    @apply flex lg:flex-row flex-col-reverse items-center justify-end;
+    border-radius: 0px 0px 0px 5000px;
+    background: url(@/assets/80042.jpg) no-repeat center center fixed;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+}
+img {
+    width: 50rem;
+    -webkit-mask-image:-webkit-gradient(linear, left bottom, left top, from(rgba(0,0,0,1)), to(rgba(0,0,0,0)));
+    mask-image: linear-gradient(to bottom, rgba(0,0,0,1), rgba(0,0,0,0));
 }
 </style>

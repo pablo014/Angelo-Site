e8b2637045496595c1405408bdd3731e2a9c27d2
pablo014 <angelopablo014@gmail.com>
2023-06-11 19:47:42 -0700

    feat(home): add side profile

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,12 +6,22 @@
       <div>
           <div class="image">
               <div class="lg:ml-10">
-                  <h1 class="lg:text-7xl text-3xl font-bold underline text-primary">
+                  <h1>
                       Hello, I'm Angelo!
                   </h1>
                   <button type="button">Download CSV</button>
               </div>
-              <img src="@/assets/angeloimg.png">
+              <img class="banner" src="@/assets/angeloimg.png">
+          </div>
+          <div class="grid grid-cols-12">
+              <div class="col-span-4">
+                  <div class="border">
+                      <img class="about-me-image" src="@/assets/side-profile-removebg-preview.png"/>
+                  </div>
+              </div>
+              <div class="col-span-8">
+                  <h1>About Me</h1>
+              </div>
           </div>
       </div>
   </main>
@@ -26,9 +36,22 @@
     -o-background-size: cover;
     background-size: cover;
 }
-img {
+.banner {
     width: 50rem;
     -webkit-mask-image:-webkit-gradient(linear, left bottom, left top, from(rgba(0,0,0,1)), to(rgba(0,0,0,0)));
     mask-image: linear-gradient(to bottom, rgba(0,0,0,1), rgba(0,0,0,0));
+}
+h1 {
+    @apply lg:text-7xl text-3xl font-bold text-primary;
+}
+.border {
+    border-radius: 500px;
+    background: linear-gradient(to bottom, #1F2833,#0B0C10, #66FCF1, #45A29E);
+    padding: .25rem;
+    margin: 1rem;
+}
+.about-me-image {
+    @apply bg-secondary rounded;
+    border-radius: 500px;
 }
 </style>
